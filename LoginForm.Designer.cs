2971a95f6765a06039d2d3315e4d855400f0be96
File <pgh268400@naver.com>
2024-05-15 01:15:41 +0900

    UI 구현 초기 로그인 화면 구현 (코드 포함)

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace POS_Project_Team2
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label_id = new Label();
+            label_pw = new Label();
+            textbox_id = new TextBox();
+            textbox_pw = new TextBox();
+            button_login = new Button();
+            button_exit = new Button();
+            button_information = new Button();
+            SuspendLayout();
+            // 
+            // label_id
+            // 
+            label_id.AutoSize = true;
+            label_id.Location = new Point(26, 26);
+            label_id.Name = "label_id";
+            label_id.Size = new Size(51, 19);
+            label_id.TabIndex = 0;
+            label_id.Text = "아이디";
+            // 
+            // label_pw
+            // 
+            label_pw.AutoSize = true;
+            label_pw.Location = new Point(26, 59);
+            label_pw.Name = "label_pw";
+            label_pw.Size = new Size(65, 19);
+            label_pw.TabIndex = 1;
+            label_pw.Text = "비밀번호";
+            // 
+            // textbox_id
+            // 
+            textbox_id.Location = new Point(108, 23);
+            textbox_id.Name = "textbox_id";
+            textbox_id.Size = new Size(161, 25);
+            textbox_id.TabIndex = 2;
+            // 
+            // textbox_pw
+            // 
+            textbox_pw.Location = new Point(108, 59);
+            textbox_pw.Name = "textbox_pw";
+            textbox_pw.PasswordChar = '*';
+            textbox_pw.Size = new Size(161, 25);
+            textbox_pw.TabIndex = 3;
+            // 
+            // button_login
+            // 
+            button_login.Location = new Point(108, 90);
+            button_login.Name = "button_login";
+            button_login.Size = new Size(91, 29);
+            button_login.TabIndex = 4;
+            button_login.Text = "로그인";
+            button_login.UseVisualStyleBackColor = true;
+            button_login.Click += button_login_Click;
+            // 
+            // button_exit
+            // 
+            button_exit.Location = new Point(205, 90);
+            button_exit.Name = "button_exit";
+            button_exit.Size = new Size(64, 29);
+            button_exit.TabIndex = 5;
+            button_exit.Text = "종료";
+            button_exit.UseVisualStyleBackColor = true;
+            button_exit.Click += button_exit_Click;
+            // 
+            // button_information
+            // 
+            button_information.Location = new Point(27, 90);
+            button_information.Name = "button_information";
+            button_information.Size = new Size(33, 29);
+            button_information.TabIndex = 6;
+            button_information.Text = "?";
+            button_information.UseVisualStyleBackColor = true;
+            button_information.Click += button_information_Click;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(322, 149);
+            Controls.Add(button_information);
+            Controls.Add(button_exit);
+            Controls.Add(button_login);
+            Controls.Add(textbox_pw);
+            Controls.Add(textbox_id);
+            Controls.Add(label_pw);
+            Controls.Add(label_id);
+            Font = new Font("맑은 고딕", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "LoginForm";
+            Text = "POS System";
+            Load += LoginForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label_id;
+        private Label label_pw;
+        private TextBox textbox_id;
+        private TextBox textbox_pw;
+        private Button button_login;
+        private Button button_exit;
+        private Button button_information;
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,87 @@
+namespace POS_Project_Team2
+{
+    /*
+       사용자 입장에서 가장 처음 만나는
+       로그인 창 (Entry Point)
+     */
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            // 실행시 창을 화면 중앙에 위치시키기
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // 디자이너 지원을 위한 함수, 절대로 이 함수를 건들지 말 것.
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            // ID, PW 입력창에 KeyPress 이벤트 핸들러 등록
+            textbox_id.KeyPress += new KeyPressEventHandler(get_enter);
+            textbox_pw.KeyPress += new KeyPressEventHandler(get_enter);
+        }
+
+        // 종료 버튼
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // 로그인 버튼
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            /*
+               외부 라이브러리를 사용할 수 없으므로,
+               DB 연결 및 로그인 처리는 문자열 평문 비교로 대체한다.
+               id : admin, pw : admin
+               양 옆에 공백이 있어도 문제 없이 로그인 되도록 Trim 처리후 비교한다.
+             */
+            if (textbox_id.Text.Trim() == "admin" && textbox_pw.Text.Trim() == "admin")
+            {
+                /*
+                  로그인 성공시 메인 화면으로 이동 :
+                  로그인 창 숨기기
+                  Close가 아닌 Hide를 하는 이유는, 본 프로그램의 시작점인 LoginForm이 닫히면
+                  프로그램이 종료되기 때문이다. 따라서 본인은 숨김 처리한다.
+                */
+                this.Hide();
+
+
+                /*
+                  여기서 메인 화면을 띄우는데 중요한 점은,
+                  메인 폼을 껐을때 프로그램이 종료되게 해야 하는데,
+                  현재 Hide된 상태의 로그인 폼을 꺼야 완전히 종료가 된다.
+                  따라서 메인 폼을 껐을때 해당 LoginForm이 종료되도록
+                  이벤트 핸들러를 걸어줘야 한다.
+                 */
+                MainForm main_form = new MainForm();
+                main_form.Closed += (s, args) => this.Close(); // important!
+                main_form.ShowDialog();
+            }
+            else
+            {
+                // 로그인 실패
+                MessageBox.Show("로그인에 실패했습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // ? 버튼 = 정보 버튼
+        private void button_information_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("아이디 / 비밀번호 분실, 사용법 및 회원가입 문의는 admin@naver.com 으로 문의 바랍니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // ID, PW 텍스트 박스에서 엔터키 입력시 수행하는 이벤트 핸들러
+        private void get_enter(object sender, KeyPressEventArgs e)
+        {
+            // 엔터키 누르면 로그인 버튼 클릭 이벤트 발생
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                button_login_Click(sender, e);
+            }
+        }
+
+
+    }
+}
